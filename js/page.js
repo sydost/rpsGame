b22fdab8b0601c94279efdbde03800638bdba11a
Sydney O <osterhsa@clarkson.edu>
2021-12-18 17:10:07 -0500

    intial

---
+++ js/page.js
@@ -0,0 +1,16 @@
+/*
+1.) Set up variables
+2.) Generate a random number
+3.) Assign number to computer pick
+4.) Register player pick
+5.) Compare computer and player pick to determine winner
+6.) Display all picks, and winner
+BONUS: Keep track of number of games (display wins, losses, ties)
+*/
+
+function simple() {
+  var test = document.getElementById("result");
+    console.log(test);
+  test.innerHTML = "Test Successful";
+  test.classList.add("red");
+}
